c44bc80a9644a363fcea1d494739b94794fbd755
Oliver Smit <oliver.smit@radically.digital>
2020-12-14 15:39:34 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,106 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
+
+import copy from "copy-to-clipboard";
+import snakeCase from "lodash/snakeCase";
+
+const TIMER_PERIOD = 3000;
 
 export default function Home() {
+  const [outputString, setOutputString] = useState("");
+  const [copiedTimeout, setCopiedTimeout] = useState(false);
+  const [output, setOutput] = useState({
+    "branch-prefix": "feature",
+    description: "I like clean code",
+  });
+
+  useEffect(() => {
+    setOutputString(
+      `git checkout -b ${output["branch-prefix"]}/${snakeCase(
+        output.description
+      )}`
+    );
+  }, [output]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCopiedTimeout(false);
+    }, TIMER_PERIOD);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [copiedTimeout]);
+
+  const onSubmit = (e) => e.preventDefault();
+  const updatePrefix = (e) => {
+    setOutput((output) => ({ ...output, [e.target.name]: e.target.value }));
+  };
+
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    inputEl.current.focus();
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Branchna.me</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <h1>Branch Name Constructor</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <form id="form" onSubmit={onSubmit} onInput={updatePrefix}>
+          <select name="branch-prefix" defaultValue={output["branch-prefix"]}>
+            <option value="feature">feature</option>
+            <option value="bug">bug</option>
+            <option value="patch">patch</option>
+          </select>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <input
+            ref={inputEl}
+            type="input"
+            name="description"
+            placeholder={output.description}
+          />
+        </form>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <hr />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            background: copiedTimeout ? "#29b5e061": "",
+          }}
+          onClick={() => {
+            setCopiedTimeout(true);
+            copy(outputString);
+          }}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          <span style={{}}>{outputString}</span>
+          <span
+            style={{
+              borderRadius: "0.5rem",
+              border: "1px solid black",
+              margin: "0.1rem",
+              padding: "0.5rem 0.25rem",
+            }}
+          >
+            {copiedTimeout ? "Copied!" : "Copy"}
+          </span>
+        </button>
+      </main>
     </div>
-  )
+  );
 }
